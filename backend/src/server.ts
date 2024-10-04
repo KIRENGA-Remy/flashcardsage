6cdc6dcdc432dcb468b0b4f3e3fe2cc02a914c48
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-10-04 11:24:39 +0200

    backend crud finished

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(cors({
   origin: "http://localhost:5173",
-  methods: ['GET', 'POST'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true
 }));
 app.use(express.json());
@@ -20,7 +20,51 @@ app.post('/api/decks', async (req: Request, res: Response) => {
     const title  = req.body.title
     const newDeck = new Decks({title});
     const createdDeck = await newDeck.save()
-    res.json(createdDeck)
+    res.json({message: "Successfully deleted" ,createdDeck})
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error', err });
+  }
+});
+
+app.get('/api/decks', async (req: Request, res: Response) => {
+  try {
+    const getDecks = await Decks.find();
+    res.json({message: "Decks are found" ,getDecks})
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error', err });
+  }
+});
+
+app.get('/api/decks/:id', async (req: Request, res: Response) => {
+  try {
+    const id  = req.params.id
+    const getDeck = await Decks.findById(id)
+    res.json({message:"Deck is found ", getDeck})
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error', err });
+  }
+});
+
+app.put('/api/decks/:id', async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    const newTitle  = req.body.title
+    const updatedDeck = await Decks.findByIdAndUpdate(
+      id, 
+      {$set: { title: newTitle }}, 
+      { new: true }
+    )
+    res.json({message: "Successfully updated" ,updatedDeck})
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error', err });
+  }
+});
+
+app.delete('/api/decks/:id', async (req: Request, res: Response) => {
+  try {
+    const id  = req.params.id
+    const deletedDeck = await Decks.findByIdAndDelete(id);
+    res.json({ message: "Successfully deleted"})
   } catch (err) {
     res.status(500).json({ message: 'Server Error', err });
   }
