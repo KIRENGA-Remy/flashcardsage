d9c8f906051b19444237723ace4acdfb5ad4e571
GITOLI Remy Claudien <gitoliremy@gmail.com>
2024-10-09 21:08:25 +0200

    header content added

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,3 +1,16 @@
+
 export function Header() {
-    return <div>Header</div>
+    return (
+        <div className="h-24 flex items-center justify-around font-bold shadow-md">
+            <div className="cursor-pointer hover:underline text-2xl">
+                <a href="/">FlashCardSage</a>
+            </div>
+            <div className="cursor-pointer hover:underline text-lg">
+            <a href="#">Decks</a>
+            </div>
+            <div className="cursor-pointer hover:underline text-lg">
+            <a href="#">Login</a>
+            </div>
+        </div>
+    )
 }
